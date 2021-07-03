ed83f5a7c3122933568675712672ed0e95fceafe
Adeshina Ibrahim <debowale01@gmail.com>
2021-07-03 11:33:08 +0100

    delete category route added

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -24,3 +24,46 @@ exports.createcategory = async (req, res, next) => {
     }
   })
 }
+
+exports.getCategory = async (req, res, next) => {
+  const {id} = req.params;
+
+  const category = await Category.findById(id);
+
+  res.status(200).json({
+    status: 'success',
+    data:{
+      category
+    }
+  })
+
+}
+exports.updateCategory = async (req, res, next) => {
+  const {id} = req.params;
+
+  const category = await Category.findById(id);
+  category.name = req.body.name;
+  //the save method is used so that the pre save hook in the model is run. 
+  await category.save();
+
+  res.status(200).json({
+    status: 'success',
+    data:{
+      category
+    }
+  })
+
+}
+exports.deleteCategory = async (req, res, next) => {
+  const {id} = req.params;
+
+  await Category.findByIdAndDelete(id);
+
+  res.status(200).json({
+    status: 'success',
+    data:{
+      category: null
+    }
+  })
+
+}
--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const { getAllCategories, createcategory } = require('../controllers/categoryController');
+const { getAllCategories, createcategory, updateCategory, getCategory, deleteCategory } = require('../controllers/categoryController');
 const router = express.Router()
 
 router.route('/').get(getAllCategories).post(createcategory);
+
+router.route('/:id').get(getCategory).patch(updateCategory).delete(deleteCategory);
 
 module.exports = router;
