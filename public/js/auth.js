35b68316e2552cb30f1bf97201763623a7568308
Adeshina Ibrahim <debowale01@gmail.com>
2021-11-11 12:46:47 +0100

    user registration

--- public/js/auth.js
+++ public/js/auth.js
@@ -25,6 +25,31 @@ export const login = async (email, password) => {
   }
 }
 
+export const register = async (name, email, password, passwordConfirm) => {
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: '/api/v1/users/signup',
+      data: {
+        name,
+        email,
+        password,
+        passwordConfirm
+      }
+    })
+
+    if(res.data.status === 'success'){
+      showAlert('success', 'Your Account was created successfully!')
+      setTimeout(() => {
+        window.location.assign('/')
+      }, 1500)
+    }
+    
+  } catch (error) {
+    showAlert('danger', error.response.data.error)
+  }
+}
+
 export const logout = async () => {
   try {
     const res = await axios({
--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,8 @@
 import '@babel/polyfill'
-import { login, logout } from './auth'
+import { login, logout, register } from './auth'
 
 const loginForm = document.getElementById('loginForm')
+const registerForm = document.getElementById('registerForm')
 const logoutBtn = document.getElementById('logout')
 
 if(loginForm){
@@ -12,6 +13,17 @@ if(loginForm){
     login(email, password)
   })
 
+}
+
+if(registerForm){
+  document.addEventListener('submit', e => {
+    e.preventDefault()
+    const name = document.querySelector('.name').value;
+    const email = document.querySelector('.email').value;
+    const password = document.querySelector('.password').value;
+    const passwordConfirm = document.querySelector('.passwordConfirm').value;
+    register(name, email, password, passwordConfirm)
+  })
 }
 
 if(logoutBtn){
