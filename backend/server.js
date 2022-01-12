a5f2512289ff1942a8ded2b214fa0d40311e0d39
Adeshina Ibrahim <debowale01@gmail.com>
2022-01-12 17:31:29 +0100

    make it better

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ mongoose.connect(DB, {
 
 
 
-const PORT = process.env.PORT || 2021;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 })
