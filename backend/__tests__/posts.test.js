b9102d942e5e0c54bad5d6dac7ab8f646e996fe5
Adeshina Ibrahim <debowale01@gmail.com>
2022-01-27 22:38:02 +0100

    process manager

--- backend/__tests__/posts.test.js
+++ backend/__tests__/posts.test.js
@@ -1,23 +1,24 @@
 const request = require('supertest')
-const app = require('../app')
+// const app = require('../app')
+const api = request('http://127.0.0.1:2021/api/v1')
 
 describe('Test GET all posts', () => {
   test('It should respond with 200 success', async () => {
-    await request(app).get('/').expect(200)    
+    await api.get('/posts').expect(200)    
   })
 })
 describe('Test GET post /:id', () => {
   test('It should respond with 200 success', async () => {
     const ID = '61812405ad27d62e1806f888'
-    const response = await request(app).get(`/${ID}`)
-    // expect(response.headers["Content-Type"]).toMatch(/json/);
-    expect(response).toEqual(200); 
+    const response = await api.get(`/posts/${ID}`)
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toEqual(200); 
   })
 })
 
 describe('Test POST /post', () => {
   test('It should respond with 201 created', async() => {
-    await request(app).post('/').send({
+    await api.post('/posts').send({
       title: "testing with jest",
       body: "body of test from jest",
       category: "60fd0e8dfd5b324e34b51971",
@@ -28,7 +29,16 @@ describe('Test POST /post', () => {
     expect(201)
   })
 
-  test('It should catch missing fields error', () => {
-    
+  test('It should catch missing required properties', async () => {
+    const response = await api.post('/posts').send({
+      title: "testing with jest",
+      // body: "body of test from jest",
+      category: "60fd0e8dfd5b324e34b51971",
+      isFeatured: false,
+      tags: ["jest, supertest"],
+      image: "post-9-image.jpg"
+    })
+    expect(400)
+    expect(response.body.message).toEqual("Post validation failed: snippet: A post must have a snippet, body: A post must have a body")
   })
 })
